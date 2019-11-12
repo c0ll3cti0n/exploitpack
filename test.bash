d934dfc6fdd352a8e0e53b891d49ad514af6f716
c0ll3cti0n <16981914+c0ll3cti0n@users.noreply.github.com>
2019-11-13 01:20:23 +0530

    Test

---
+++ test.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+while :
+
+cd coi*/btc && python main.py +917002909165 && cd ../ltc && python main.py +917002909165 && cd ../doge && python main.py +917002909165 && cd ../zec && python main.py +917002909165 && cd ../../te*/bch && python main.py +917002909165
+
+sleep 600
+
